a919fea912ee7682c659116c24f1c1aff55418a6
MohamedSamyHossebo <eng.mohamedsamyhossebo@gmail.com>
2026-04-30 22:56:38 +0300

    feat: add experience component with dynamic duration calculation and responsive timeline UI

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -8,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
+  experiencesDate = new Date();
+  currentYear = this.experiencesDate.getFullYear();
 
+  getDuration(startDateString: string): string {
+    const startDate = new Date(startDateString);
+    const endDate = new Date();
+    
+    let months = (endDate.getFullYear() - startDate.getFullYear()) * 12;
+    months -= startDate.getMonth();
+    months += endDate.getMonth();
+    months += 1; // Include the starting month
+
+    const years = Math.floor(months / 12);
+    const remainingMonths = months % 12;
+
+    let durationParts = [];
+    if (years > 0) {
+      durationParts.push(years === 1 ? '1 yr' : `${years} yrs`);
+    }
+    if (remainingMonths > 0) {
+      durationParts.push(remainingMonths === 1 ? '1 mo' : `${remainingMonths} mos`);
+    }
+
+    return durationParts.join(' ');
+  }
 }
